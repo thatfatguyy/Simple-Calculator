f61a85a69d44e442576a1a8bb03f132b6ce32a65
Uday Kumar <udaykumar4u07@gmail.com>
2026-01-16 12:22:50 +0545

    Intial  Commit

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-a= int(input("Enter a number:"))
-b= int (input("Enter a number:"))
+decision=""
 
-print("""ENTER:
-      A:Addition
-      S:Substraction
-      M:Multiplication
-      D:Division""")
+while (decision != "n"):
+    try:
+        a= int(input("Enter a number:"))
+        b= int (input("Enter a number:"))
+        print("""\nPress:
+        A:Addition
+        S:Substraction
+        M:Multiplication
+        D:Division\n""")
 
-choice=input("Enter the operation you want to do:").lower()
+        choice=input("Enter the operation you want to do:").lower()
+        match choice:
+            case "a":
+                print(f"\nThe sum of {a} and {b} is {a+b}\n")
 
-match choice:
-    case "a":
-        print(f"The sum of {a} and {b} is {a+b}")
+            case "s":
+                print(f"\nThe difference of {a} and {b} is {a-b}\n")
 
-    case "s":
-        print(f"The difference of {a} and {b} is {a-b}")
+            case "m":
+                print(f"\nThe product of {a} and {b} is {a*b}\n")
 
-    case "m":
-        print(f"The product of {a} and {b} is {a*b}")
+            case "d":
+                try:
+                    print(f"\nThe quoitent of {a} and {b} is {a/b}\n")
+                except ZeroDivisionError as e:
+                    print("\nCannot be divided by 0.Enter a valid divider\n")
 
-    case "d":
-        try:
-            print(f"The quoitent of {a} and {b} is {a/b}")
-        except ZeroDivisionError as e:
-            print(" Cannot be divided by 0.Enter a valid divider")
+            case _:
+                print("\nSomething went wrong!!!\n")
+    except ValueError :
+        print("\nEnter a valid number.")
 
-    case _:
-        print("Something went wrong!!!")
+ 
+        
+    print("""\nDo you want to continue for more operations:
+                    Press \"Y\" for Yes
+                    Press \"N\" for No\n""")
+    decision=input("Enter your decision:").lower()
 
 
 
